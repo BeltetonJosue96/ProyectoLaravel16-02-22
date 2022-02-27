86b934fd8bed19683a54ac7c6a2f6bdbeb36190b
BeltetonJosue96 <josuebelteton@icloud.com>
2022-02-26 19:47:29 -0600

    Realizo los factories, y ejecuto el seeder por 1000 registros

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Student;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class StudentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Student::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'Nombres'=>$this->faker->firstName(),
+            'PrimerApellido'=>$this->faker->lastName(),
+            'SegundoApellido'=>$this->faker->lastName(),
+            'CorreoElectronico'=>$this->faker->freeEmail(),
+            'Celular'=>$this->faker->phoneNumber,
+            'Direccion'=>$this->faker->address(),
+            'Roles'=>$this->faker->randomElement(['Estudiante', 'Repitente', 'Catedratico', 'Coordinador', 'Direcctivo'])
+        ];
+    }
+}
